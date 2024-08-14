018d780126252af785a9950611865c9f5a5e95b7
Mikhail Mahamood <mikhailmahamood@gmail.com>
2024-08-14 16:56:01 +0800

    added sprite folder and drumsticks

--- Graphics.lua
+++ Graphics.lua
@@ -1,8 +1,12 @@
 Graphics = {}
 
 function Graphics:load()
-    frozen_patty = love.graphics.newImage('frozen_patty.png')
-    cooked_patty = love.graphics.newImage('cooked_patty.png')
-    burnt_patty = love.graphics.newImage('burnt_patty.png')
+    frozen_patty = love.graphics.newImage('sprites/frozen_patty.png')
+    cooked_patty = love.graphics.newImage('sprites/cooked_patty.png')
+    burnt_patty = love.graphics.newImage('sprites/burnt_patty.png')
+
+    raw_drum = love.graphics.newImage('sprites/rawChickenDrumstick.png')
+    egg_drum = love.graphics.newImage('sprites/eggChickenDrumstick.png')
+    flour_drum = love.graphics.newImage('sprites/flourChickenDrumstick.png')
 end
 
